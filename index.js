ec9ff19c17cfd1cadd2cb17b9cb3b53945c31df2
BarnacleJones <benmjones@protonmail.com>
2022-07-13 13:47:18 +1200

    starting to build website

---
+++ index.js
@@ -0,0 +1,37 @@
+
+
+const API_KEY = '2fa068cac41b91284b8fae107e0f1ed9';
+
+const IPAPI_KEY = "3fd9ffc5508c476d8e46b68a707d3028"
+
+
+//set up city for easy changing if pi changes location
+let city = "Tauranga";
+const cityName = document.getElementById("city");
+cityName.innerText = city;
+
+let tempField = document.getElementById("textfieldTemp");
+let pressureField = document.getElementById("pressureField");
+let humidityField = document.getElementById("humidityField");
+
+
+document.getElementById("time").innerText = formattedTime;
+async function getWeather(city){
+    try {          
+    const weather = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`, {mode: 'cors'});
+    const data = await weather.json();   
+    let temp = data.main.temp;
+    let pressure = data.main.pressure;
+    let humidity = data.main.humidity;
+    tempField.innerText = `Temperature: ${temp} degrees (celcius)`; 
+    pressureField.innerText = `Pressure: ${pressure}`;
+    humidityField.innerText = `Humidity: ${humidity}`;
+
+}
+    catch (error) {
+        tempField.innerText = "404 error invalid API call";
+    }
+    
+}
+
+getWeather(city);
