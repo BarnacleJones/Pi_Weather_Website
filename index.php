0d4d8811d601e9a56247decc1317c4fd98660f35
BarnacleJones <benmjones@protonmail.com>
2022-07-15 08:56:29 +1200

    troubleshooting/sending back and forth to server

--- index.php
+++ index.php
@@ -70,17 +70,17 @@
     echo ($result);
 
 
-    $row = mysqli_fetch_row($result);
-    echo ("<div class='card'>
-                <div class='card-body'>
-                  <h2 class='card-title'>Timestamp: $row[3]</h2>
-                  <ul class='list-group list-group-flush'>
-                      <li class='list-group-item'>Temperature: $row[0]</li>
-                      <li class='list-group-item'>Pressure: $row[1]</li>
-                      <li class='list-group-item'>Humidity:$row[2]</li>
-                  </ul>
-                </div>
-              </div>");
+    // $row = mysqli_fetch_row($result);
+    // echo ("<div class='card'>
+    //             <div class='card-body'>
+    //               <h2 class='card-title'>Timestamp: $row[3]</h2>
+    //               <ul class='list-group list-group-flush'>
+    //                   <li class='list-group-item'>Temperature: $row[0]</li>
+    //                   <li class='list-group-item'>Pressure: $row[1]</li>
+    //                   <li class='list-group-item'>Humidity:$row[2]</li>
+    //               </ul>
+    //             </div>
+    //           </div>");
     // }
     $connection->close();
 
