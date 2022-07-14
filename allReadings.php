f8339b1157fd6d1b434a355581a80e936114dfd7
BarnacleJones <benmjones@protonmail.com>
2022-07-14 17:16:41 +1200

    database populating allReadings page

--- allReadings.php
+++ allReadings.php
@@ -9,7 +9,6 @@
   <script src="index.js" type="module" defer></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   <link rel="stylesheet" href="style.css">
-
 </head>
 
 
@@ -23,8 +22,8 @@
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
           <a class="nav-link" href="./index.php">Home</a>
-          <a class="nav-link active" aria-current="page" href="#">About</a>
-          <a class="nav-link" href="./allReadings.php">All Readings</a>
+          <a class="nav-link active" href="./about.html">About</a>
+          <a class="nav-link" aria-current="page" href="#">All Readings</a>
         </div>
       </div>
     </div>
