63883488d4e7669d145b21c73deb7b55e4555165
BarnacleJones <benmjones@protonmail.com>
2022-07-14 17:09:06 +1200

    first try

--- allReadings.php
+++ allReadings.php
@@ -54,16 +54,16 @@
 
     while ($row = mysqli_fetch_row($result)) {
       echo "
-      '<div class='card'>
+      <div class='card'>
          <div class='card-body'>
            <h2 class='card-title'>Timestamp: ' . $row[3] . '</h2>
            <ul class='list-group list-group-flush'>
-            <li class='list-group-item'>Temperature: ' . $row[0] . '</li>
-            <li class='list-group-item>Pressure: ' . $row[1] . '</li>
-           <li class='list-group-item'>Humidity: ' . $row[2] . '</li>
+              <li class='list-group-item'>Temperature: ' . $row[0] . '</li>
+              <li class='list-group-item>Pressure: ' . $row[1] . '</li>
+              <li class='list-group-item'>Humidity: ' . $row[2] . '</li>
            </ul>
-           </div>
-           </div>";
+          </div>
+        </div>";
     }
     $mysqli->close();
 
