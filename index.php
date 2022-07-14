51379d18742b534ead601e1b8120b176a2f40e4a
BarnacleJones <benmjones@protonmail.com>
2022-07-15 08:38:34 +1200

    troubleshooting/sending back and forth to server

--- index.php
+++ index.php
@@ -6,8 +6,6 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Pi Weather Station</title>
-
-  <script src="index.js" type="module" defer></script>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
@@ -39,19 +37,19 @@
   </div>
   <div class="container">
     <?php
-    // // Database credentials
-    // $user = 'root';
-    // $pw = 'raspberry pi yum yum';
-    // $db = 'sense_data';
+    // Database credentials
+    $user = 'root';
+    $pw = 'raspberry pi yum yum';
+    $db = 'sense_data';
 
-    // //Database connection
-    // $connection = new mysqli('localhost', $user, $pw, $db);
+    //Database connection
+    $connection = new mysqli('localhost', $user, $pw, $db);
 
-    // //error if connection fails
-    // if ($connection->connect_errno) {
-    //   printf("Connect failed: %s\n", $connection->connect_error);
-    //   exit();
-    // }
+    //error if connection fails
+    if ($connection->connect_errno) {
+      printf("Connect failed: %s\n", $connection->connect_error);
+      exit();
+    }
     // //variable that returns all records in database
     // //creates a connection object
     // $result = $connection->query('SELECT TOP 1 * FROM data ORDER BY TIMESTAMP desc');
@@ -86,13 +84,13 @@
       </div>
     </div>
 
-    <script type="module">
-      import date from './node_modules/date-and-time/esm/date-and-time.es.min.js';
-      const now = new Date();
-      document.getElementById("time").innerText = date.format(now, 'hh:mm:s:SS A [GMT]Z');
-    </script>
-
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+<script src="index.js" type="module"></script>
+<script type="module">
+  import date from './node_modules/date-and-time/esm/date-and-time.es.min.js';
+  const now = new Date();
+  document.getElementById("time").innerText = date.format(now, 'hh:mm:s:SS A [GMT]Z');
+</script>
 
 </html>
